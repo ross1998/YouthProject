cc7797eddb116803f9c79d448f2e3ad8c14610e7
Ross Martsenyak <Ross@rostislavs-macbook-air.local>
2018-12-15 18:18:39 -0800

    Initial Commit

---
+++ Redeemed Youth App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Redeemed Youth App
+//
+//  Created by Rostislav Martsenyak on 12/15/18.
+//  Copyright © 2018 Ross M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
