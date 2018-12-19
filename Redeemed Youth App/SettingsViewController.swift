642e26f48f2af47463db789e87f43589dd2ba5f8
Roman Kucheryavyy <romanhazard@romans-mac.local>
2018-12-18 18:00:26 -0800

    Added another view controller where we can put our settings in, as well as customized the layout and stuff.

---
+++ Redeemed Youth App/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  Redeemed Youth App
+//
+//  Created by Roman Kucheryavyy on 12/18/18.
+//  Copyright © 2018 Ross M. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
